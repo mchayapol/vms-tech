037bf2f37ecfb44f9426a4a6d99600a87c1fc97a
Chayapol Moemeng <mchayapol@gmail.com>
2023-06-13 20:47:39 +0700

    Changed to bubble sort

--- hello.py
+++ hello.py
@@ -1,2 +1,12 @@
-print("Hello")
-print("World")
+L = [15,6,7,89,54,76,8,98,32]
+
+def bubble_sort(A):
+    for i in range(len(A)):
+        for j in range(len(A)-1):
+            if A[j] > A[j+1]:
+                A[j],A[j+1] = A[j+1],A[j]
+    return A
+
+print(L)
+bubble_sort(L)
+print(L)
